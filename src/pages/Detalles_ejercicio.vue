6f868501c085a260007da0e706a330fdd4fd56ea
DiegoHill <diegillos@alum.us.es>
2022-03-07 16:54:04 +0100

    Feat: Vista detalles ejercicio #59
    Disposición y placeholders

---
+++ src/pages/Detalles_ejercicio.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="grid">
+        <div class="col-12 lg:col-6 xl:col-4">
+            <div class="card mb-0">
+                <div class="text-900 font-medium text-xl">Vídeo</div>
+            </div>
+        </div>
+        <div class="col-12 lg:col-6 xl:col-4">
+            <div class="card mb-0">
+                <div class="text-900 font-medium text-xl">Nombre del ejercicio</div>
+                <br>
+                <div class="text-900 font-small text-xl">Calorías</div>
+                <div class="text-900 font-small text-xl">Material</div>
+                <div class="text-900 font-small text-xl">Repeticiones</div>
+                <div class="text-900 font-small text-xl">Tiempo</div>
+                <div class="text-900 font-small text-xl">Dificultad</div>
+                <br>
+                <div class="flex align-items-right justify-content-right">
+                    <Button label="Añadir a recomendados" class="p-button-success mr-2 mb-2" />
+                </div>
+            </div>
+            <br>
+            <div class="card mb-0">
+                <div class="text-900 font-medium text-xl">Imagen grupos musculares</div>
+            </div>
+        </div>
+        <div class="col-12 lg:col-6 xl:col-4">
+            <div class="card mb-0">
+                    <div class="text-900 font-medium text-xl">Descripción</div>
+            </div>
+        </div>
+    </div>
+</template>
