f8c25a7360ebfe82a88965660ba7ccd7d998c1cf
DiegoHill <diegillos@alum.us.es>
2022-03-09 13:59:52 +0100

    Feat: Vista detalles ejercicio #59
    Cosas que faltaban en la vista.

--- src/pages/Detalles_ejercicio.vue
+++ src/pages/Detalles_ejercicio.vue
@@ -1,11 +1,17 @@
 <template>
 	<div class="grid">
-        <div class="col-12 lg:col-6 xl:col-4">
+        <div class="col-12 lg:col-2 xl:col-2">
+        <h1>Detalles</h1>
+        </div>
+        <div class="col-12 lg:col-10 xl:col-10">
+        <Button icon="pi pi-times" class="p-button-rounded p-button-danger p-button-outlined mr-2 mb-2 lado_derecho" />
+        </div>
+        <div class="col-12 lg:col-6 xl:col-3">
             <div class="card mb-0">
                 <div class="text-900 font-medium text-xl">Vídeo</div>
             </div>
         </div>
-        <div class="col-12 lg:col-6 xl:col-4">
+        <div class="col-12 lg:col-6 xl:col-3">
             <div class="card mb-0">
                 <div class="text-900 font-medium text-xl">Nombre del ejercicio</div>
                 <br>
@@ -19,15 +25,64 @@
                     <Button label="Añadir a recomendados" class="p-button-success mr-2 mb-2" />
                 </div>
             </div>
-            <br>
+        </div>
+         <div class="col-12 lg:col-6 xl:col-3">
             <div class="card mb-0">
                 <div class="text-900 font-medium text-xl">Imagen grupos musculares</div>
             </div>
         </div>
-        <div class="col-12 lg:col-6 xl:col-4">
+        <div class="col-12 lg:col-6 xl:col-3">
             <div class="card mb-0">
                     <div class="text-900 font-medium text-xl">Descripción</div>
             </div>
         </div>
+        <div class="col-12 lg:col-6 xl:col-4">
+
+        </div>
+        <div class="col-12 lg:col-6 xl:col-12">
+        <h1>Ejercicios similares</h1>
+        </div>
+        <div class="col-12 lg:col-6 xl:col-4">
+            <div class="card mb-0">
+                <div class="text-900 font-medium text-xl">Nombre del ejercicio</div>
+                <br>
+                <div class="text-900 font-small text-xl">Calorías</div>
+                <div class="text-900 font-small text-xl">Material</div>
+                <div class="text-900 font-small text-xl">Repeticiones</div>
+                <div class="text-900 font-small text-xl">Tiempo</div>
+                <div class="text-900 font-small text-xl">Dificultad</div>
+            </div>
+        </div>
+        <div class="col-12 lg:col-6 xl:col-4">
+            <div class="card mb-0">
+                <div class="text-900 font-medium text-xl">Nombre del ejercicio</div>
+                <br>
+                <div class="text-900 font-small text-xl">Calorías</div>
+                <div class="text-900 font-small text-xl">Material</div>
+                <div class="text-900 font-small text-xl">Repeticiones</div>
+                <div class="text-900 font-small text-xl">Tiempo</div>
+                <div class="text-900 font-small text-xl">Dificultad</div>
+            </div>
+        </div>
+        <div class="col-12 lg:col-6 xl:col-4">
+            <div class="card mb-0">
+                <div class="text-900 font-medium text-xl">Nombre del ejercicio</div>
+                <br>
+                <div class="text-900 font-small text-xl">Calorías</div>
+                <div class="text-900 font-small text-xl">Material</div>
+                <div class="text-900 font-small text-xl">Repeticiones</div>
+                <div class="text-900 font-small text-xl">Tiempo</div>
+                <div class="text-900 font-small text-xl">Dificultad</div>
+            </div>
+        </div> 
     </div>
 </template>
+
+<style>
+        h1 {
+            font-size: 30px;
+        }
+        .lado_derecho {
+            float: right;
+        }
+</style>
