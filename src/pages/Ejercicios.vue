dbaa650ed3e9b65f377bc28bafc78ab1d8141596
Ángel Torregrosa Domínguez <angtordom1@alum.us.es>
2022-03-28 22:50:45 +0200

    Feat: Realizar primera versión vista ejercicios

--- src/pages/Ejercicios.vue
+++ src/pages/Ejercicios.vue
@@ -1,80 +1,54 @@
 <template>
-	<div class="grid">			
-
-	<div class="col-12 xl:col-6">
-		<h3>EJERCICIOS RECOMENDADOS DEL DÍA </h3>
-		<div class="col-12">
-			<div class="tabla">
-        <br>
-        <DataTable :value="dataviewValue" :rows="5">
-                      
-          <Column field="name" header="Nombre" :style="{width:'150px'}">
-          
-						<template #body="{data}">
-              <router-link :to="'/ejercicio/detalles/' + data._id">{{data.name}}</router-link>
-            </template>
-
-					</Column>
-          
-
-          <Column field="muscles" header="Zona muscular" :style="{width:'150px'}">
-						<template #body="{data}" :v-model="muscleList">
-              <span class="text-bold">{{muscleList[data.muscles[0]]}}   
-                <br>{{muscleList[data.muscles[1]]}}
-                <br>{{muscleList[data.muscles[2]]}}
-              </span> 
-            </template>
-					</Column>
-
-        </DataTable>
-
-        
-			</div>
-		</div>
-	</div>
-	
-	
-	<div class="col-12 xl:col-6">
-		<h3>BUSCADOR DE EJERCICIOS</h3>
-		<div class="col-12">
-      <div class="contenedor">
-				<router-link to= "/ejercicio">
-          <img src="../assets/images/foto.png"/>
-					<div class="texto-centrado"> En esta sección podrás buscar a partir de unos filtros establecidos.</div>
-				</router-link>
+  <div class="grid">
+    <!-- PARTE IZQUIERDA -->
+    <div class="col-12 lg:col-6">
+      <div class="grid col-12 justify-content-center align-items-center"  style="margin-bottom:1em">
+        <div class="col-12">
+          <h2 class="text-center"> EJERCICIOS RECOMENDADOS DEL DÍA </h2>
+        </div>
+        <div class="card">
+          <DeferredContent @load="fetchItems">
+            <DataView :value="dataviewValue" :layout="layout">
+              <template #list="slotProps">
+                <div class="card col-12 pt-1 pb-1">
+                  <div class="formgroup-inline">
+                    <div class="field ml-5">
+                      <Galleria :value="slotProps.data.images" :numVisible="1" containerStyle="max-width: 150px; max-height:150px;"
+                          :circular="true" :autoPlay="true" :transitionInterval="1500">
+                          <template #item="slotProps">
+                              <img :src="slotProps.item"  style="width: 90px; height:90px; display: block;" />
+                          </template>
+                          <template #thumbnail="slotProps">
+                            <img src="" :alt="slotProps.item.alt" style="display: block;" />
+                        </template>
+                      </Galleria>
+                    </div>
+                    <div class="field">
+                      {{slotProps.data.name}}
+                    </div>
+                  </div>
+                </div>
+              </template>
+              <template #empty>No tiene ningún ejercicio recomendado</template>
+            </DataView>
+          </DeferredContent>
+        </div>    
       </div>
-            
     </div>
-		
-	</div>
-	
+    <!-- PARTE DERECHA -->
+    <div class="col-12 lg:col-6">
+      <div class="grid col-12 justify-content-center align-items-center">
+        <div class="col-12">
+          <h2 class="p-flex text-center">BUSCADOR DE EJERCICIOS</h2>
+        </div>
+        <router-link to= "/ejercicio">
+          <img src='../../public/images/buscador_ejercicios.png' alt='Buscador de Ejercicios' class="img-fluid"/>
+        </router-link>
+        <h4 class="p-flex text-center"> En esta sección podrás buscar a partir de unos filtros establecidos.</h4>
+      </div>
+    </div>
 	</div>
 </template>
-
-
-
-<style>
-
-.contenedor{
-    position: relative;
-    display: inline-block;
-    text-align: center;
-}
-
-.texto-centrado{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-	color:rgb(0, 0, 0);
-	background-color: rgb(202, 211, 203);
-	opacity: .8;
-	font-weight: bold;
-	font-size: large;
-}
-</style>
-
-
 <script>
 import axios from "axios"
 
@@ -111,17 +85,25 @@ export default {
     
     fetchItems(){
           let uri = '/ejercicios';
-          axios.get(uri).then((response) => {
-          this.dataviewValue = response.data;
-          console.log(this.dataviewValue);
+          axios.get(uri)
+          .then((response) => {
+            this.dataviewValue = response.data;
+            console.log(this.dataviewValue);
           });
     },
     exercise_url(url){
         return url._id;
     }
-
-    
-
   },
 };
 </script>
+
+<style>
+.p-galleria .p-galleria-thumbnail-container{
+  display: none;
+}
+.img-fluid{
+  width:100%;
+  height: auto;
+}
+</style>
