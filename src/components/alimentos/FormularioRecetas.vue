ca8d31e05140b67551f1ade3f6cd014d8bba18c5
DiegoHill <diegillos@alum.us.es>
2022-03-30 17:41:55 +0200

    Feat: Implementar recetas #123
    Se ha implementado una versión preliminar de la validación del formulario de creación/actualización de recetas.

--- src/components/alimentos/FormularioRecetas.vue
+++ src/components/alimentos/FormularioRecetas.vue
@@ -11,11 +11,13 @@
 				<div class="p-fluid formgrid grid">
 					<div class="field col-12 md:col-6">
 						<label for="nombre">Nombre*</label>
-						<InputText id="nombre" v-model.trim="receta.nombre" required="true" autofocus />
+						<InputText id="nombre" v-model.trim="receta.nombre" required="true" autofocus :class="{'p-invalid': enviado && errorNombre}" />
+						<small class="p-invalid" v-if="enviado && errorNombre">{{this.errorNombre}}</small>
 					</div>
 					<div class="field col-12 md:col-6">
 						<label for="raciones">Raciones*</label>
-						<InputNumber id="raciones" v-model="receta.raciones" :min="0" required="true" autofocus />
+						<InputNumber id="raciones" v-model="receta.raciones" :min="0" required="true" autofocus :class="{'p-invalid': enviado && errorRaciones}" />
+						<small class="p-invalid" v-if="enviado && errorRaciones">{{this.errorRaciones}}</small>
 					</div>
 				</div>
 
@@ -26,10 +28,12 @@
                         </div>
                         <div class="p-fluid formgrid grid" v-for="(ing,indiceIngredientes) in receta.ingredientes" :key="indiceIngredientes">
                             <div class="field col-12 md:col-9">
-                                <Dropdown id="ingrediente" v-model="ing.ingrediente" :options="alimentos" optionLabel="nombre" required="true" autofocus />
+                                <Dropdown id="ingrediente" v-model="ing.ingrediente" :options="alimentos" optionLabel="nombre" required="true" autofocus :class="{'p-invalid': enviado && errorIngrediente}" />
+								<small class="p-invalid" v-if="enviado && errorIngrediente">{{this.errorIngrediente}}</small>
                             </div>
                             <div class="field col-12 md:col-3">
-                                <InputText id="cantidad" v-model="ing.cantidad" required="true" autofocus />
+                                <InputText id="cantidad" v-model="ing.cantidad" required="true" autofocus :class="{'p-invalid': enviado && errorCantidad}" />
+								<small class="p-invalid" v-if="enviado && errorCantidad">{{this.errorCantidad}}</small>
                             </div>
                             <div class="field col-12 md:col-10 md:col-offset-1">
                                 <Button class="p-button-success"  @click="anyadirIngrediente(indiceIngredientes)"  v-show="indiceIngredientes == receta.ingredientes.length-1">
@@ -46,7 +50,8 @@
 
                         <div class="formgrid grid justify-content-center" v-for="(paso,indicePasos) in receta.pasos" :key="indicePasos">
                             <div class="field col-12">
-                                <InputText id="pasos" v-model="receta.pasos[indicePasos]" required="true" autofocus />
+                                <InputText id="pasos" v-model="receta.pasos[indicePasos]" required="true" autofocus :class="{'p-invalid': enviado && errorPaso}" />
+								<small class="p-invalid" v-if="enviado && errorPaso">{{this.errorPaso}}</small>
                             </div>
                             <div class="field col-12 md:col-10 md:col-offset-1">
                                 <Button class="p-button-success" @click="anyadirPaso(indicePasos)" v-show="indicePasos == receta.pasos.length-1">
@@ -118,42 +123,84 @@ export default {
 			this.receta.pasos.push('');
 		},
 		guardarReceta () {
-		this.enviado = true;
+			this.enviado = true;
 
-		//HACER COMPROBACIONES PERTINENTES DE LOS CAMPOS
-		//let comprobado = this.comprobarCampos();
-		let comprobado = true;
+			//HACER COMPROBACIONES PERTINENTES DE LOS CAMPOS
+			let comprobado = this.comprobarCampos();
 
-		if (comprobado) {
-			console.log(this.receta);
-			for (var ingrediente of this.receta.ingredientes) {  
-			ingrediente = ingrediente.id;
+			if (comprobado) {
+				console.log(this.receta);
+				for (var ingrediente of this.receta.ingredientes) {  
+				ingrediente = ingrediente.id;
+				}
+				if (this.receta._id) {
+					this.recetaService.actualizarReceta(this.receta)
+					.then(() => {
+						//REDIRIGIR A LA PAGINA DE LISTADO CON UN TOAST DE CONFIRMACIÓN
+						this.$router.push({ name: 'comidas', params: {mensaje: 'alimentoModificado'} });
+					})
+					.catch((e)=>{
+						//SI OCURRE ALGÚN FALLO AL INSERTAR EN LA BD, MOSTRAR
+						console.log('error' + e);
+						this.$toast.add({severity:'error', summary: 'Fallo', detail: 'No se ha podido actualizar la receta, lo sentimos.', life: 3000});
+					})
+				}else{
+					this.recetaService.guardarReceta(this.receta)
+					.then(() => {
+						//REDIRIGIR A LA PAGINA DE LISTADO CON UN TOAST DE CONFIRMACIÓN
+						this.$router.push({ name: 'comidas', params: {mensaje: 'alimentoInsertado'} });
+					})
+					.catch((e)=>{
+						//SI OCURRE ALGÚN FALLO AL INSERTAR EN LA BD, MOSTRAR
+						console.log('error' + e);
+						this.$toast.add({severity:'error', summary: 'Fallo', detail: 'No se ha podido guardar la receta, lo sentimos.', life: 3000});
+					})
+				}
 			}
-			if (this.receta._id) {
-				this.recetaService.actualizarReceta(this.receta)
-				.then(() => {
-					//REDIRIGIR A LA PAGINA DE LISTADO CON UN TOAST DE CONFIRMACIÓN
-					this.$router.push({ name: 'comidas', params: {mensaje: 'alimentoModificado'} });
-				})
-				.catch((e)=>{
-					//SI OCURRE ALGÚN FALLO AL INSERTAR EN LA BD, MOSTRAR
-					console.log('error' + e);
-					this.$toast.add({severity:'error', summary: 'Fallo', detail: 'No se ha podido actualizar la receta, lo sentimos.', life: 3000});
-				})
+		},
+		comprobarCampos() {
+			let regexLetrasNumeros = /^[A-zÀ-ÿ0-9 _]+$/;
+			//let regexLetras = /^[A-zÀ-ÿ]+$/;
+			let resultado = true;
+			if(!this.receta.nombre || !regexLetrasNumeros.test(this.receta.nombre))
+			{
+				resultado = false;
+				this.errorNombre = 'Debe introducir un nombre y solo puede usar letras y números';
+			} 
+			if (!this.receta.raciones || this.receta.raciones<=0)
+			{
+				resultado = false;
+				this.errorRaciones = 'Debe especificar el número de raciones';
+			}
+			if(this.receta.ingredientes.lenght == 0){
+				resultado = false;
+				this.errorIngrediente = 'Debe introducir al menos un ingrediente';
+			}
+			else{
+				for (var ing of this.receta.ingredientes) {  
+					if (!ing.ingrediente || !regexLetrasNumeros.test(ing.ingrediente)) {
+							resultado = false;
+							this.errorIngrediente = 'Debe introducir ingredientes válidos';
+					}
+					if (!ing.cantidad || !regexLetrasNumeros.test(ing.cantidad)) {
+							resultado = false;
+							this.errorCantidad = 'Debe especificar una cantidad con letras y números';
+					}
+				}
+			}
+			if(this.pasos.lenght == 0){
+				resultado = false;
+				this.errorPaso = 'Debe indicar al menos un paso y los pasos solo pueden contener letras y números';
 			}else{
-				this.recetaService.guardarReceta(this.receta)
-				.then(() => {
-					//REDIRIGIR A LA PAGINA DE LISTADO CON UN TOAST DE CONFIRMACIÓN
-					this.$router.push({ name: 'comidas', params: {mensaje: 'alimentoInsertado'} });
-				})
-				.catch((e)=>{
-					//SI OCURRE ALGÚN FALLO AL INSERTAR EN LA BD, MOSTRAR
-					console.log('error' + e);
-					this.$toast.add({severity:'error', summary: 'Fallo', detail: 'No se ha podido guardar la receta, lo sentimos.', life: 3000});
-				})
+				for (var paso of this.receta.pasos) {
+					if (!paso || !regexLetrasNumeros.test(paso)) {
+						resultado = false;
+						this.errorPaso = 'Debe indicar al menos un paso y los pasos solo pueden contener letras y números';
+					}
+				}
 			}
-		}
-	},
+			return resultado;
+		},
 		volver () {
 			this.enviado = true;
 			location.href = "/alimentos_recetas";
