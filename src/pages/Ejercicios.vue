ddff8963f6ecfa19aa4bd99ae840867a6b292b62
Ángel Torregrosa Domínguez <angtordom1@alum.us.es>
2022-04-02 12:01:49 +0200

    Fix: Arreglar ejercicios

--- src/pages/Ejercicios.vue
+++ src/pages/Ejercicios.vue
@@ -1,26 +1,31 @@
 <template>
-  <div class="grid justify-content-around">
+  <div class="grid justify-content-between">
     <!-- PARTE IZQUIERDA -->
-    <div class="grid col-12 lg:col-6 justify-content-center">
+    <div class="col-12 lg:col-6 justify-content-center">
         <div class="col-12">
           <h3 class="text-center"> EJERCICIOS RECOMENDADOS DEL DÍA </h3>
         </div>
         <div class="card col-12">
-        <DataTable :value="dataviewValue" :rows="5">     
-          <Column field="name" header="Nombre" :style="{width:'150px'}">
+        <DataTable  :value="dataviewValue" :rows="7" responsiveLayout="scroll"> 
+  
+          <Column field="name" header="Nombre" :style="{width:'50%'}" >
 						<template #body="{data}">
               <router-link :to="'/ejercicio/detalles/' + data._id">
               <p class="p-flex">{{data.name}}</p>
               </router-link>
             </template>
 					</Column>
-          <Column field="muscles" header="Zona muscular" :style="{width:'150px'}">
+          <Column field="muscles" header="Zona muscular" :style="{width:'50%'}">
 						<template #body="{data}" :v-model="muscleList">
-                <p class="p-flex text-bold">{{muscleList[data.muscles[0]]}}</p> 
-                <p class="p-flex text-bold">{{muscleList[data.muscles[1]]}}</p>
-                <p class="p-flex text-bold">{{muscleList[data.muscles[2]]}}</p>
+                <p class="p-flex text-bold inline" v-for="(musculo,index) in data.muscles" :key="musculo">
+                  <template v-if="index > 0">, </template> 
+                  {{muscleList[musculo]}}
+                </p> 
             </template>
 					</Column>
+          <template #empty>
+            <p class="text-center p-flex"> No tiene ninguna recomendación de ejercicios para hoy. </p>
+          </template>
         </DataTable>
       </div>    
     </div>
@@ -32,7 +37,9 @@
         <router-link to= "/ejercicio">
           <img src='../../public/images/buscador_ejercicios.png' alt='Buscador de Ejercicios' class="img-fluid"/>
         </router-link>
-        <h5 class="p-flex text-center">En esta sección podrás buscar a partir de unos filtros establecidos</h5>
+        <div class="col-12">
+          <h5 class="p-flex text-center">En esta sección podrás buscar a partir de unos filtros establecidos</h5>
+        </div>
     </div>
 	</div>
 </template>
@@ -71,7 +78,7 @@ export default {
   methods: {
     
     fetchItems(){
-          let uri = '/ejercicios';
+          let uri = '/ejercicios/recomendacion';
           axios.get(uri)
           .then((response) => {
             this.dataviewValue = response.data;
@@ -79,7 +86,7 @@ export default {
     },
     exercise_url(url){
         return url._id;
-    }
+    },
   },
 };
 </script>
