34c4ee49f0c9345a03b184921493740ab0510e6b
Beatriz Llamas <beallasai@alum.us.es>
2022-04-03 19:51:24 +0200

    Feat: avances CRUD ejercicios #133

--- src/service/ExerciseService.js
+++ src/service/ExerciseService.js
@@ -2,6 +2,14 @@ import axios from 'axios';
 
 export default class ExerciseService {
 
+    getExercises(){
+        return axios.get('/ejercicios')
+        .then((response) => response.data)
+        .catch((e)=>{
+            console.log('error' + e);
+        });
+    }
+
     saveExercise(exercise){
         return axios.post('/done_exercise/', exercise);
     }
