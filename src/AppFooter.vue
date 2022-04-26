1cdcb688800ee9992e770dc8938473cb2cd311ab
Mariano Manuel Torrado Sánchez <marianomtorrados@gmail.com>
2022-04-26 19:05:31 +0200

    Feature: Navbar mejorado #137

--- src/AppFooter.vue
+++ src/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="layout-footer" v-if="$route.fullPath !== '/login' && $route.fullPath !== '/register' && $route.fullPath !== '/condiciones'">
 		<router-link to="/GDPR">
-                <p style="color:gray; font-size: 1.5em;">Aviso legal</p>
+			<p style="color:gray; font-size: 1.5em;">Aviso legal</p>
         </router-link>
 	</div>
 </template>
